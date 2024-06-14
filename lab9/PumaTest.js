0a7913298d381fdfdbeffe0a6e5803519bda63a6
Ivanova Sofia <sofiaigorevna@outlook.com>
2024-06-15 00:58:08 +0300

    Сохранение изменений перед переключением на main

--- lab9/PumaTest.js
+++ lab9/PumaTest.js
@@ -3,10 +3,8 @@ const { Builder, By, until } = require('selenium-webdriver');
 async function run() {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
-    // Переход на сайт
     await driver.get('https://www.lamoda.by/');
 
-    // Переход на страницу с каталогом товаров "Обувь"
     const shoesCategoryLinkSelector = 'a[href="/c/15/shoes-women/?sitelink=topmenuW&l=3"]';
     await driver.wait(until.elementLocated(By.css(shoesCategoryLinkSelector)), 15000);
     console.log('Ссылка на категорию "Обувь" найдена');
@@ -16,13 +14,9 @@ async function run() {
     console.log('Переход на страницу с обувью');
 
     await driver.wait(until.elementLocated(By.css('div._content_pjvgk_16')), 15000);
-
-    // Нахождение и нажатие на кнопку для открытия фильтра по бренду
     const brandFilterButton = await driver.findElement(By.xpath("//div[@class='_text_pjvgk_26']//span[text()='Бренд']"));
     await brandFilterButton.click();
     console.log('Нажата кнопка для открытия фильтра по бренду');
-
-
 
 
     const brandSearchInputSelector = 'input._input_u3foj_9';
@@ -32,20 +26,16 @@ async function run() {
     console.log('Введено "PUMA" в поле поиска бренда');
 
 
-// Дадим время на обновление результатов
+    await driver.sleep(5000); 
 
-    await driver.sleep(5000); // Дадим время на обновление результатов
-
-    // Обновленный XPath для поиска чекбокса
     const pumaCheckboxXPath = "//span[text()='PUMA']/ancestor::div[contains(@class, 'x-checkbox')]//input[@type='checkbox']";
     await driver.wait(until.elementLocated(By.xpath(pumaCheckboxXPath)), 10000);
     const pumaCheckbox = await driver.findElement(By.xpath(pumaCheckboxXPath));
     console.log('Чекбокс PUMA найден');
 
-    // Проверка, выбран ли чекбокс
+
     const isChecked = await pumaCheckbox.getAttribute('aria-checked');
     if (isChecked !== 'true') {
-      // Изменение атрибута aria-checked с помощью JavaScript и вызов клика на родительском элементе
       await driver.executeScript(`
         const checkbox = arguments[0];
         checkbox.setAttribute('aria-checked', 'true');
@@ -61,7 +51,6 @@ async function run() {
 
     await driver.sleep(1000);
 
-    // Найти и кликнуть кнопку "Применить" (если такая есть)
     const applyButtonExists = await driver.findElements(By.css('button._apply_1x1qp_20'));
     if (applyButtonExists.length > 0) {
       const applyButton = await driver.findElement(By.css('button._apply_1x1qp_20'));
@@ -69,10 +58,8 @@ async function run() {
       console.log('Нажата кнопка "Применить"');
     }
 
-    // Подождать немного, чтобы результаты могли обновиться
     await driver.sleep(3000);
 
-    // Проверка, что вся обувь отображаема действительно PUMA
     const productBrandNames = await driver.findElements(By.css('div.x-product-card-description__brand-name._brandName_1rcja_6.x-product-card-description__brand-name_faded'));
     let allPuma = true;
     for (let brandNameElement of productBrandNames) {
--- lab9/sizeTest.js
+++ lab9/sizeTest.js
@@ -3,10 +3,8 @@ const { Builder, By, until } = require('selenium-webdriver');
 async function run() {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
-    // Переход на сайт
     await driver.get('https://www.lamoda.by/');
 
-    // Закрытие всплывающего уведомления, если оно появляется
     const closeNotificationButtonSelector = 'section._notifyForm_1x9k4_2._notifyBar_1h0rw_2 button';
     const notificationElements = await driver.findElements(By.css(closeNotificationButtonSelector));
     if (notificationElements.length > 0) {
@@ -15,7 +13,6 @@ async function run() {
       console.log('Закрыто всплывающее уведомление');
     }
 
-    // Переход на страницу с каталогом товаров "Обувь"
     const shoesCategoryLinkSelector = 'a[href="/c/15/shoes-women/?sitelink=topmenuW&l=3"]';
     await driver.wait(until.elementLocated(By.css(shoesCategoryLinkSelector)), 15000);
     console.log('Ссылка на категорию "Обувь" найдена');
@@ -26,20 +23,17 @@ async function run() {
 
     await driver.wait(until.elementLocated(By.css('div._content_pjvgk_16')), 15000);
 
-    // Нахождение и нажатие на кнопку для открытия фильтра по размеру
     const brandFilterButton = await driver.findElement(By.xpath("//div[@class='_text_pjvgk_26']//span[text()='Размер']"));
     await brandFilterButton.click();
     console.log('Нажата кнопка для открытия фильтра по размеру');
 
-    await driver.sleep(1000); // Дадим время на обновление результатов
+    await driver.sleep(1000);
 
-    //XPath для поиска чекбокса
     const sizeCheckboxXPath = "//span[text()='48,5']/ancestor::div[contains(@class, 'x-checkbox')]//input[@type='checkbox']";
     await driver.wait(until.elementLocated(By.xpath(sizeCheckboxXPath)), 10000);
     const sizeCheckbox = await driver.findElement(By.xpath(sizeCheckboxXPath));
     console.log('Чекбокс 48,5 найден');
 
-    // Проверка, выбран ли чекбокс
     const isChecked = await sizeCheckbox.getAttribute('aria-checked');
     if (isChecked !== 'true') {
       // Изменение атрибута aria-checked с помощью JavaScript и вызов клика на родительском элементе
@@ -58,7 +52,6 @@ async function run() {
 
     await driver.sleep(1000);
 
-    // Найти и кликнуть кнопку "Применить" (если такая есть)
     const applyButtonExists = await driver.findElements(By.css('button._apply_1x1qp_20'));
     if (applyButtonExists.length > 0) {
       const applyButton = await driver.findElement(By.css('button._apply_1x1qp_20'));
@@ -66,10 +59,8 @@ async function run() {
       console.log('Нажата кнопка "Применить"');
     }
 
-    // Подождать немного, чтобы результаты могли обновиться
     await driver.sleep(3000);
- 
-    // Проверка наличия размера 48.5 на карточках товаров
+    
 const productCardSelector = 'div.x-product-card__card';
 const productCards = await driver.findElements(By.css(productCardSelector));
 
